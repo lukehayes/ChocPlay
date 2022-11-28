bc8dda050802dd297b0241a2017b8100f212a4e2
Luke Hayes <lukepurplehayes@gmail.com>
2022-11-28 12:25:40 +0000

    Added extra entity for testing.

--- src/Main.hx
+++ src/Main.hx
@@ -11,11 +11,16 @@ class Main extends hxd.App {
     override function init() {
         world = World.instance;
 
-        var e = new sim.BasicEntity();
-        e.addComponent("Transform", new sim.components.TransformComponent());
-
-        world.addEntity(1, e);
         world.addSystem(new sim.system.BasicSystem(this));
+
+        var e1 = new sim.BasicEntity();
+        e1.addComponent("Transform", new sim.components.TransformComponent(30,30));
+        world.addEntity(1, e1);
+
+        var e2 = new sim.BasicEntity();
+        e2.addComponent("Transform", new sim.components.TransformComponent(130,330));
+        world.addEntity(2, e2);
+
     }
 
     override function update(dt:Float) 
